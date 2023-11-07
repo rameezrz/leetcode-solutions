5c6929f0accf0ea84dd2e4fc5dfe7eef0fd071b2
rzeee <rameez007rz@gmail.com>
2023-11-07 10:03:05 +0530

    58, 1903. Largest Odd Number in String

---
+++ 58_leetcode.js
@@ -0,0 +1,8 @@
+const num = "10133890";
+const largestOddNumber = (num) => {
+  for (let i = num.length - 1; i >= 0; i--)
+    if (num[i] % 2 !== 0) return num.slice(0, i + 1);
+  return "";
+};
+
+console.log(largestOddNumber(num));
